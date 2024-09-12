25f0e9deb1c1107f6772fbc2ac062b22e538d063
Will <Will@MacBook-Pro-4.hsd1.nj.comcast.net>
2024-09-11 21:54:16 -0400

    Init Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("lock combination");
+let text ="You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+//Variables for the combination
+const first = 9+1;
+const second = 30+10;
+const third = 40-1;
+//Pop up Alert
+alert(
+    `The combination for the lock is ${first} - ${second} - ${third}`
+);
+    
